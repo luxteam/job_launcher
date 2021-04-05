4287b10fd94764bce15ea4fcf75ca3a60f112443
Ilia Nemankov <INemankov@luxoft.com>
2021-04-05 10:00:00 +0300

    Fix make_redirect_page.sh script

--- make_redirect_page.sh
+++ make_redirect_page.sh
@@ -3,4 +3,4 @@ REDIRECT_LINK="$1"
 OUTPUT_PATH="$2"
 FILE_NAME="$3"
 
-python3 core/make_redirect_page.py --redirect_link "$REDIRECT_LINK" --output_path "$OUTPUT_PATH" --file_name "$FILE_NAME"
+python core/make_redirect_page.py --redirect_link "$REDIRECT_LINK" --output_path "$OUTPUT_PATH" --file_name "$FILE_NAME"
